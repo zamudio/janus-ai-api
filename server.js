597a103705633f678136e8a0b0fd33d6d68af1eb
github username <github email address>
2020-05-22 02:41:41 -0700

    server host connection

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-infinite-72719",
     user: "michaelzamudio",
     password: "",
     database: "janus",
